01224739691c124699ee7a432a26b1d4fa220f1b
Dmitry K <mr.demyan77@gmail.com>
2022-02-05 08:51:44 +0300

    create

---
+++ admin/login.php
@@ -0,0 +1,5 @@
+<form method="post">
+    <input name="login" type="text" placeholder="Login">
+    <input name="password" type="password">
+    <button name="auth">Login</button>
+</form>
---
+++ admin/parse.ini.php
@@ -0,0 +1,5 @@
+<?php
+
+$iniArr = parse_ini_file('./config.ini');
+
+print_r($iniArr);
